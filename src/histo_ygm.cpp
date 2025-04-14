0bc3475419b8b2506b180e3a855fb3b8c84932a4
ryan-dozier <ry482284@ucf.edu>
2025-04-14 11:13:31 -0400

    memcpy tracking

--- src/histo_ygm.cpp
+++ src/histo_ygm.cpp
@@ -166,6 +166,8 @@ int main(int argc, char **argv) {
     output["SHM_SKIP_COUNT"]               = boost::json::array();
     output["SHM_WAIT_TIME_SUM"]            = boost::json::array();
     output["SHM_WAIT_TIME_MAX"]            = boost::json::array();
+    output["SHM_MEMCPY_TIME_SUM"]          = boost::json::array();
+    output["SHM_MEMCPY_TIME_MAX"]          = boost::json::array();
     output["TABLE_SIZE"]                   = global_table_size;
     output["INSERTIONS"]       = params.local_updates * world.size();
     output["REDUCING_ADAPTER"] = params.use_reducing_adapter;
